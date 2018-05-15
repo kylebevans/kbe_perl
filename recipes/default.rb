1e34cb8053fbbbccfe00b495fef512a05e305a8d
Kyle Evans <evans.584@osu.edu>
2018-05-15 19:06:32 -0400

    update packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,8 @@
 # Copyright Holder Email:: kylebe@gmail.com
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'libwww-perl'
+package 'libcrypt-ssleay-perl'
+package 'uwsgi'
+package 'uwsgi-plugin-psgi'
