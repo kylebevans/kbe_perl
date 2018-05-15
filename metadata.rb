24bef1578233d34502f321daa8d9f5ace17faa20
Kyle Evans <evans.584@osu.edu>
2018-05-15 15:48:03 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'kbe_perl'
+maintainer 'Kyle Evans'
+maintainer_email 'kylebe@gmail.com'
+license 'all_rights'
+description 'Installs/Configures kbe_perl'
+long_description 'Installs/Configures kbe_perl'
+version '0.1.0'
